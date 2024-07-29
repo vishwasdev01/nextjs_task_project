c4e668e11b9b5ac1b69d32175db4aa35ea2aad6e
vishwas <vishwasdev01@gmail.com>
2024-07-29 22:37:08 +0530

    deploy changes

--- src/ApiRqquest.js/index.tsx
+++ src/ApiRqquest.js/index.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:3000'
+// const BASE_URL = 'http://localhost:3000'
+const BASE_URL = '/'
 const axiosInstance = axios.create({ baseURL: BASE_URL })
 interface USER {
   first_name: string
